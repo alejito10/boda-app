34a8c13f0237a7f6a528c471e905e212598e07ef
Alejandro González <alejandre.gp@hotmail.com>
2020-11-24 10:56:30 +0100

    creadas carpetas nuevas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import firebase from "firebase";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>hola</p>
     </div>
   );
 }
