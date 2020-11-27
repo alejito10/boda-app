3bb29772c6d7ba26768efb8d2de1f7c5b72c0ba2
Alejandro González <alejandre.gp@hotmail.com>
2020-11-27 11:06:59 +0100

    cambios en el formulario, font para el titulo

--- src/pages/formulario/Formulario.jsx
+++ src/pages/formulario/Formulario.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
+import axios from 'axios'
 import { useForm } from "react-hook-form";
 import './formulario.css'
 
 export function Formulario() {
     const { register, handleSubmit, errors } = useForm();
-    const onSubmit = (data) => {
+    const onSubmit = (data,e) => {
 			console.log("DATOS", data);
+			axios.post("https://back-boda-app.herokuapp.com/new",data).then(res=>{
+				//respuesta del servidor
+				console.log(res);
+				if(res.status==200){
+					e.target.reset();
+				}
+			});
+			
+			
 		};
     return (
 			<div>
+				<h2 className='subtitulo'>¿Alergias o intolerancias?</h2>
+				<p className="texto-alergias">
+					Queremos que disfrutéis de nuestro día al máximo y cómo os merecéis,
+					así que si tenéis alguna alergia o intolerancia, 
+					<br/>tendremos todo
+					preparado para que no os tengáis que preocupar de nada.<br/> Sólo tenéis
+					que rellenar el formulario a continuación y el staff de Valquijancho
+					se encargará de todo.
+				</p>
 				<div className="c-formulario">
 					<form onSubmit={handleSubmit(onSubmit)} className="b-form">
 						<div className="c-contenido">
@@ -25,7 +44,7 @@ export function Formulario() {
 									ref={register({ required: true })}
 									placeholder="Nombre"
 								/>
-								{errors.name && <span>This field is required</span>}
+								{errors.name && <span>Este campo es requerido</span>}
 							</label>
 						</div>
 						<div className="c-contenido">
@@ -44,14 +63,12 @@ export function Formulario() {
 										pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
 									})}
 								/>
-								{errors.email && (
-									<span>This field is required and need to be an email</span>
-								)}
+								{errors.email && <span>Este campo es requerido</span>}
 							</label>
 						</div>
 						<div className="c-contenido">
 							<div>
-								<span>Vegano</span>
+								<span>Vegano/Vegetariano</span>
 							</div>
 							<select name="vegano" id="vegano" ref={register}>
 								<option value="no">No</option>
@@ -74,12 +91,10 @@ export function Formulario() {
 								</div>
 
 								<textarea
-									className="b-input"
+									className="b-input2"
 									name="alergias"
 									id="alergias"
-									cols="30"
-									rows="5"
-									ref={register({ required: true })}
+									ref={register}
 								/>
 								{errors.message && <span>This field is required</span>}
 							</label>
--- src/shared/components/titulo/Titulo.jsx
+++ src/shared/components/titulo/Titulo.jsx
@@ -3,8 +3,8 @@ import './titulo.css'
 
 export function Titulo() {
     return (
-        <div className='title'>
-            <h1>Boda Natalia & Alejandro</h1>
+        <div className='c-title'>
+            <h1 className='title'>Boda Natalia & Alejandro</h1>
         </div>
     )
 }
